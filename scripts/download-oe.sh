d355f1fbc7cc81a27cd3833cd73cc502e14f7d6e
Ruben Dröge <rdroge@progress.com>
2024-12-20 15:51:56 +0100

    Changed build context (workspace dir) and speficied specific Dockerfile

--- scripts/download-oe.sh
+++ scripts/download-oe.sh
@@ -3,6 +3,7 @@
   ls ${PWD}
   docker run -v ${PWD}/build/installer:/target progressofficial/oe_installer:$1
   ls -l ${PWD}/build/installer
-  mv ${PWD}/build/installer/PROGRESS_OE.tar.gz ${PWD}/build/installer/PROGRESS_PATCH_OE.tar.gz
+  cp ${PWD}/build/installer/PROGRESS_OE.tar.gz ${PWD}/build/installer/PROGRESS_PATCH_OE.tar.gz
+  rm ${PWD}/build/installer/PROGRESS_OE.tar.gz
   docker run -v ${PWD}/build/installer:/target progressofficial/oe_installer:$2
   ls -l ${PWD}/build/installer
