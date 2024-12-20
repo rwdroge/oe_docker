40c6ad39e53a6047cb3d73d3e74c4018c461faeb
Ruben Dröge <rdroge@progress.com>
2024-12-20 16:30:35 +0100

    Why is the move failing of the Progress installer (permissions)

--- scripts/download-oe.sh
+++ scripts/download-oe.sh
@@ -3,7 +3,6 @@
   ls ${PWD}
   docker run -v ${PWD}/build/installer:/target progressofficial/oe_installer:$1
   ls -l ${PWD}/build/installer
-  cp ${PWD}/build/installer/PROGRESS_OE.tar.gz ${PWD}/build/installer/PROGRESS_PATCH_OE.tar.gz
-  rm ${PWD}/build/installer/PROGRESS_OE.tar.gz
+  sudo mv ${PWD}/build/installer/PROGRESS_OE.tar.gz ${PWD}/build/installer/PROGRESS_PATCH_OE.tar.gz
   docker run -v ${PWD}/build/installer:/target progressofficial/oe_installer:$2
   ls -l ${PWD}/build/installer
