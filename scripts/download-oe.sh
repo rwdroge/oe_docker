2f74b299fa386aa991a39a5dcf0dafe4ebdf4f51
Ruben Dröge <rdroge@progress.com>
2024-12-20 16:58:00 +0100

    Quick change, removed 12.2 for now because of lacking INI files

--- scripts/download-oe.sh
+++ scripts/download-oe.sh
@@ -1,8 +1,7 @@
-  echo version=$1
+  echo oeversion=$1
   echo release=$2
-  ls ${PWD}
+  
   docker run -v ${PWD}/build/installer:/target progressofficial/oe_installer:$1
-  ls -l ${PWD}/build/installer
   sudo mv ${PWD}/build/installer/PROGRESS_OE.tar.gz ${PWD}/build/installer/PROGRESS_PATCH_OE.tar.gz
   docker run -v ${PWD}/build/installer:/target progressofficial/oe_installer:$2
   ls -l ${PWD}/build/installer
