6775b125a4122f40ba66e5986ac121bbe2f64ff6
Ruben Dröge <rdroge@progress.com>
2024-12-20 10:59:37 +0100

    Struggling with the download

--- scripts/download-oe.sh
+++ scripts/download-oe.sh
@@ -1,4 +1,7 @@
-  echo docker run -v ${PWD}/src:/binaries/oe progressofficial/oe_installer:${{ env.OE_VERSION}}
+  echo base: ${OE_VERSION}
+  echo version: ${env.OE_VERSION}
+  
+  docker run -v ${PWD}/src:/binaries/oe progressofficial/oe_installer:${OE_VERSION}
   mv ${PWD}/src/PROGRESS_OE.tar.gz ${PWD}/src/PROGRESS_PATCH_OE.tar.gz
-  echo docker run -v ${PWD}/src:/binaries/oe progressofficial/oe_installer:${{ env.OE_RELEASE}}
+  docker run -v ${PWD}/src:/binaries/oe progressofficial/oe_installer:${{ env.OE_RELEASE}}
   ls -l ${PWD}/src
