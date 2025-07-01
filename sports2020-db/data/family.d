91c7b036e84898f2a18ad9cc73dccd61acb4371d
Ruben Dröge <rdroge@progress.com>
2025-07-01 14:40:14 +0200

    Sports2020db & .NET 8 added to pasoe dev

---
+++ sports2020-db/data/family.d
@@ -0,0 +1,84 @@
+1 "Jenny Koberlein" "Daughter" 01/15/18 yes 01/15/18
+1 "Ken Koberlein" "Spouse" 12/07/85 yes 03/16/18
+2 "Bill Andrews" "Son" 12/21/10 yes 11/03/17
+2 "Sam Andrews" "Spouse" 05/07/83 yes 01/06/18
+3 "Kelly Smith" "Daughter" 08/18/13 yes 06/19/17
+3 "Mark Smith" "Spouse" 01/08/80 yes 02/21/18
+3 "Sam Smith" "Son" 05/18/10 yes 05/17/18
+4 "Jenny Shaw" "Daughter" 09/19/17 yes 01/29/18
+4 "Sam Shaw" "Son" 03/16/15 no 01/29/18
+4 "Sue Shaw" "Spouse" 09/26/85 yes 01/29/18
+5 "Jason Bradford" "Son" 06/18/95 yes 11/24/17
+5 "Ken Bradford" "Spouse" 12/04/70 yes 02/21/18
+6 "Fran Petersen" "Spouse" 01/09/95 yes 02/05/18
+6 "Jack Petersen" "Son" 08/17/18 yes 08/17/18
+7 "Ken Weiss" "Son" 08/05/15 no 01/29/18
+8 "Kathy Kelly" "Daughter" 09/18/15 no 09/18/18
+8 "Mary Kelley" "Spouse" 03/18/80 yes 01/01/18
+9 "Mary Wilson" "Daughter" 08/18/15 no 12/06/17
+9 "Sandra Wilson" "Spouse" 04/05/90 yes 01/29/18
+10 "Amy Davis" "Spouse" 09/09/75 yes 02/01/18
+10 "Mark Davis" "Son" 01/27/05 no 01/27/18
+11 "Gretta Garsen" "Spouse" 09/09/90 yes 11/30/34
+12 "Robert Morris" "Spouse" 09/18/68 yes 03/20/18
+12 "Stan Morris" "Son" 05/13/89 yes 10/27/17
+13 "Ellen Sanders" "Daughter" 09/17/17 no 10/22/17
+13 "Karen Sanders" "Spouse" 09/09/87 yes 03/30/18
+14 "Douge Adams" "Spouse" 09/29/78 yes 02/07/18
+15 "Alex Simon" "Daughter" 09/17/10 yes 10/20/17
+15 "Shelly Simons" "Spouse" 09/08/81 yes 03/18/18
+16 "Philip Atkins" "Spouse" 09/09/83 yes 03/09/18
+17 "Clara Barry" "Spouse" 08/07/82 yes 03/22/18
+17 "Harry Barry" "Son" 09/18/12 yes 10/14/17
+17 "Linda Barry" "Daughter" 08/12/10 yes 10/14/17
+18 "Jean Brady" "Daughter" 09/17/17 no 09/30/17
+18 "Mark Brady" "Spouse" 11/12/86 yes 01/08/18
+19 "Barbra Dawsen" "Spouse" 09/09/86 yes 02/19/18
+20 "Kate Flanagan" "Spouse" 09/09/80 yes 03/19/18
+20 "Zoe Flanagan" "Dauhter" 01/21/12 yes 04/19/17
+21 "Laura Burton" "Daughter" 09/12/16 yes 09/12/18
+21 "Lisa Burton" "Spouse" 09/09/81 yes 01/08/18
+21 "Sam Burton" "Son" 09/12/18 yes 09/12/18
+22 "John Green" "Spouse" 03/09/86 yes 12/28/17
+22 "Martin Green" "Son" 09/12/16 no 06/03/17
+25 "Megan Davis" "Daughter" 10/02/15 no 12/20/17
+25 "Rodger Davis" "Son" 02/21/17 yes 12/20/17
+27 "Margo Meader" "Daughter" 09/18/07 no 01/15/18
+28 "Andy Parker" "Son" 10/02/10 no 04/13/17
+30 "Ken Raymond" "Son" 09/12/10 no 08/09/17
+30 "Mark Raymond" "Son" 09/12/10 no 08/09/17
+32 "Ben Shultz" "Son" 01/20/10 no 03/21/18
+32 "Mary Shultz" "Daughter" 12/09/12 no 03/21/18
+33 "Matthew Shay" "Son" 01/28/12 no 07/11/17
+33 "Scott Shay" "Son" 08/14/10 no 07/11/17
+35 "Shelby Snyder" "Daughter" 02/12/12 no 04/29/17
+38 "Ron Tedford" "Son" 01/28/10 no 07/25/17
+39 "Sandy Valentino" "Daughter" 02/21/15 no 04/02/17
+44 "Bill Young" "Son" 08/12/15 no 02/18/18
+44 "Kelly Young" "Daughter" 09/12/18 no 09/12/18
+46 "Don Abbott" "Son" 05/18/16 yes 10/19/17
+46 "Shelly Abbott" "Daughter" 12/09/15 no 10/19/17
+47 "Sam Anderson" "Son" 02/16/16 no 03/27/18
+48 "Tom Alden" "Son" 09/18/18 yes 09/18/18
+49 "Jenny Ahern" "Daughter" 12/14/17 no 12/14/17
+49 "Larry Ahern" "Son" 09/12/15 no 08/17/17
+50 "Ellen Berry" "Daughter" 09/08/98 no 08/19/17
+50 "Joe Berry" "Son" 02/16/00 yes 08/19/17
+52 "Mark Brown" "Son" 09/16/12 no 07/13/17
+52 "Meghan Brown" "Daughter" 09/12/15 no 07/13/17
+53 "Fran Burton" "Daughter" 08/17/10 no 12/01/17
+54 "Kyle Kayhill" "Son" 02/17/15 no 02/17/18
+55 "Carrie Caine" "Daughter" 03/12/15 no 08/31/17
+55 "Kelsey Caine" "Daughter" 11/01/13 no 08/31/17
+.
+PSC
+filename=Family
+records=0000000000072
+ldbname=sports2020
+timestamp=2025/06/25-08:55:35
+numformat=44,46
+dateformat=mdy-1950
+map=NO-MAP
+cpstream=UTF-8
+.
+0000003469
